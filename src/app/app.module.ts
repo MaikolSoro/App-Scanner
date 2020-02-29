be19c38f82a26e5bd230ff244c6409060d36e2f3
MaikolSoro <46753453+MaikolSoro@users.noreply.github.com>
2020-02-29 13:39:32 -0600

    Se agrego la opción de enviar por correo, Se creo el archivo csv y enviar correo electronico con el archivo generado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,8 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { IonicStorageModule } from '@ionic/storage';
 
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-
+import { File } from '@ionic-native/file/ngx';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -24,6 +25,8 @@ import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
     SplashScreen,
     BarcodeScanner,
     InAppBrowser,
+    File,
+    EmailComposer,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/services/data-local.service.ts
+++ src/app/services/data-local.service.ts
@@ -3,6 +3,8 @@ import { Registro } from '../models/registro.model';
 import { Storage } from '@ionic/storage';
 import { NavController } from '@ionic/angular';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,8 +14,11 @@ export class DataLocalService {
 
    constructor(private storage: Storage, 
                private navCtrl: NavController, 
-               private iab: InAppBrowser) {
-        this.cargarStorage();
+               private iab: InAppBrowser,
+               private file: File,
+               private emailComposer: EmailComposer) {
+
+              this.cargarStorage();
    }
 
    // cargo la información del storage
@@ -50,4 +55,53 @@ export class DataLocalService {
 
   }
   }
+
+  // metodo para enviar correo, configurando la data
+  enviarCorreo() {
+  const arrTemp = [];
+  const titulos = 'Tipo, Formato, Creado en, Texto\n';
+  arrTemp.push(titulos);
+  this.guardados.forEach( registro => {
+    const linea =   `${registro.type}, ${registro.format}, ${registro.created}, ${registro.text.replace(',', '')}\n`;
+
+    arrTemp.push(linea);
+  });
+
+  this.crearArchivoFisico(arrTemp.join(''));
+}
+
+// crear el archivo fisico
+  crearArchivoFisico(text: string) {
+    this.file.checkFile(this.file.dataDirectory, 'registros.csv').then(existe => {
+      return this.escribirEnArchivo( text);
+    }).catch(err => {
+      return this.file.createFile(this.file.dataDirectory, 'registros.csv', false).
+      then(creado => this.escribirEnArchivo(text))
+      .catch(err2 => console.log('No se pudo crear el archivo', err2));
+    });
+}
+  async escribirEnArchivo(text: string) {
+
+    const archivo = `${this.file.dataDirectory}/registros.csv`;
+    await this.file.writeExistingFile(this.file.dataDirectory, 'registros.csv', text);
+
+    const email = {
+      to: 'maikolsoro.z1998@gmail.com',
+      //cc: 'erika@mustermann.de',
+     // bcc: ['john@doe.com', 'jane@doe.com'],
+     attachments: [
+      archivo
+    ],
+    subject: 'Backup de scans',
+    body: 'Aquí tienen sus backups de los scans - <strong>ScanApp</strong>',
+    isHtml: true
+  };
+
+  // Send a text message using default options
+    this.emailComposer.open(email);
+
+}
+
+
+
 }
